468952674bc34ae7ebff68553201f5b113303798
Reese Moore <ram@vt.edu>
2011-10-09 21:55:23 -0400

    Create a listing of strings to be used by the database.

---
+++ OutputMessages.java
@@ -0,0 +1,20 @@
+/**
+ * Store the output messages from the Database.
+ * 
+ * @author Reese Moore
+ * @author Tyler Kahn
+ * @version 2011.10.09
+ */
+public class OutputMessages {
+	public static String InsertBadX = "Insert rejected, bad X coordinate";
+	public static String InsertBadY = "Insert rejected, bad Y coordinate";
+	public static String InsertSuccess = "Insert operation successful";
+	public static String InsertDup = "Insert rejected, coordinates duplicate an existing city record";
+	public static String MakeNullSuccess = "Makenull operation successful";
+	public static String RemoveNoFound = "No such City Record found";
+	public static String RemoveBadX = "Remove operation failed: Bad X coordinate";
+	public static String RemoveBadY = "Remove operation failed: Bad Y coordinate";
+	public static String SearchBadX = "Search operation failed: Bad X value";
+	public static String SearchBadY = "Search operation failed: Bad Y value";
+	public static String SearchBadRadius = "Search operation failed: Bad radius value";
+}
