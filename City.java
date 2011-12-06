dcc2e138e4545ed04410cef5d7ea192dfcea9223
Reese Moore <ram@vt.edu>
2011-12-05 19:14:10 -0500

    Make the 'City' class capable of allocating, dereferencing, and freeing city records.

--- City.java
+++ City.java
@@ -12,6 +12,106 @@ public class City {
 	private final String name;
 	
 	/**
+	 * Allocate a new city record, store it in the memory pool, and return a 
+	 * handle to the memory.
+	 * @param mem The memory manager to use to store this city
+	 * @param x The x coordinate
+	 * @param y The y coordinate
+	 * @param name The name of the city
+	 */
+	public static Handle alloc(MemoryManager mem, int x, int y, String name)
+	{
+		byte[] city_record = new byte[12];
+
+		// Store the name separately.
+		Handle city_name = mem.insert(name.getBytes(), name.length());
+		
+		// Store the x
+		city_record[0] = (byte) (x & 0xFF000000);
+		city_record[1] = (byte) (x & 0x00FF0000);
+		city_record[2] = (byte) (x & 0x0000FF00);
+		city_record[3] = (byte) (x & 0x000000FF);
+		
+		// Store the y
+		city_record[4] = (byte) (y & 0xFF000000);
+		city_record[5] = (byte) (y & 0x00FF0000);
+		city_record[6] = (byte) (y & 0x0000FF00);
+		city_record[7] = (byte) (y & 0x000000FF);
+		
+		// Store the pointer
+		int n = city_name.getOffset();
+		city_record[8]  = (byte) (n & 0xFF000000);
+		city_record[9]  = (byte) (n & 0x00FF0000);
+		city_record[10] = (byte) (n & 0x0000FF00);
+		city_record[11] = (byte) (n & 0x000000FF);
+		
+		// Store in the memory pool and return a handle to it.
+		return mem.insert(city_record, 12);
+	}
+	
+	/**
+	 * Dereference a 'pointer' to a city and return a reconstructed City object
+	 * @param mem The memory manager to read from
+	 * @param handle The Handle to dereference
+	 */
+	public static City deref(MemoryManager mem, Handle handle)
+	{
+		// Get the data from the memory manager
+		byte[] city_record = new byte[12];
+		mem.get(handle, city_record, 12);
+		
+		int x = 0x00000000;
+		x |= (city_record[0] << 3);
+		x |= (city_record[1] << 2);
+		x |= (city_record[2] << 1);	
+		x |= (city_record[3] << 0);
+		
+		int y = 0x00000000;
+		y |= (city_record[4] << 3);
+		y |= (city_record[5] << 2);
+		y |= (city_record[6] << 1);
+		y |= (city_record[7] << 0);
+		
+		int n = 0x00000000;
+		n |= (city_record[8] << 3);
+		n |= (city_record[9] << 2);
+		n |= (city_record[10] << 1);
+		n |= (city_record[11] << 0);
+		
+		// Dereference the name
+		byte[] name_dat = new byte[256];
+		int sz = mem.get(new Handle(n), name_dat, 256);
+		byte[] name_dat_pack = new byte[sz];
+		System.arraycopy(name_dat, 0, name_dat_pack, 0, sz);
+		String name = new String(name_dat_pack);
+		
+		return new City(x, y, name);
+	}
+	
+	/**
+	 * Free a pointer to a City object
+	 * @param mem The memory manager to work with.
+	 * @param handle The handle to free
+	 */
+	public static void free(MemoryManager mem, Handle handle)
+	{	
+		// Get the data from the memory manager
+		byte[] city_record = new byte[12];
+		mem.get(handle, city_record, 12);
+		
+		// Get the pointer to the string.
+		int n = 0x00000000;
+		n |= (city_record[8] << 3);
+		n |= (city_record[9] << 2);
+		n |= (city_record[10] << 1);
+		n |= (city_record[11] << 0);
+		
+		// Free the memory
+		mem.remove(new Handle(n));
+		mem.remove(handle);
+	}
+	
+	/**
 	 * Create a new city record.
 	 * @param x The x coordinate of the city.
 	 * @param y The y coordinate of the city.
